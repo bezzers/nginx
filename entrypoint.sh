a335ef025452a8da096b9c3a70f8c25c75a718e2
bezzers <bezzers@users.noreply.github.com>
2016-03-17 08:59:28 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ curl -k -L -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://$VAULT_SECRET_URL/ngi
 curl -k -L -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://$VAULT_SECRET_URL/nginx | jq -r '.data.cert' | sed 's/"//g' > /etc/nginx/cert.crt
 
 # Populate the template for the configuration file with the relevant environment variables
-envsubst < /etc/nginx/nginx-template.conf > /etc/nginx/nginx.conf
+envsubst '$SERVICE_ADDRESS $PUBLIC_ADDRESS' < /etc/nginx/nginx-template.conf > /etc/nginx/nginx.conf
 
 # Start nginx
-nginx -g daemon off;
+nginx
