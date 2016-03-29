7ef6f49f98fba4667a9769633449c02b19f41068
bezzers <bezzers@users.noreply.github.com>
2016-03-29 10:54:11 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 # -k allows us to trust the certificates without verifying them
 # -L allows us to be redirected between vault servers
 # jq lets us parse the json, -r makes sure new line characters come through properly, sed removes surrounding quotes
-curl -k --location-trusted -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://$VAULT_SECRET_URL/nginx | jq -r '.data.key' | sed 's/"//g' > /etc/nginx/cert.key
-curl -k --location-trusted -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://$VAULT_SECRET_URL/nginx | jq -r '.data.cert' | sed 's/"//g' > /etc/nginx/cert.crt
+curl -k -L -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://$VAULT_SECRET_URL/nginx | jq -r '.data.key' | sed 's/"//g' > /etc/nginx/cert.key
+curl -k -L -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://$VAULT_SECRET_URL/nginx | jq -r '.data.cert' | sed 's/"//g' > /etc/nginx/cert.crt
 
 # Populate the template for the configuration file with the relevant environment variables
 envsubst '$SERVICE_ADDRESS:$PUBLIC_ADDRESS' < /etc/nginx/nginx-template.conf > /etc/nginx/nginx.conf
